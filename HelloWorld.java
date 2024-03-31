bf7fc47fb1d37ebe976e9efe66baf4863b5b95e8
2718684668@qq.com <2718684668@qq.com>
2024-03-31 20:46:12 +0800

    first

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.*;
+//1:无需package
+//2: 类名必须Main, 不可修改
+
+public class Main {
+           private void mian() {
+	// TODO 自动生成的方法存根
+	 System.out.println("Hello World!");
+  
+     }
+}
